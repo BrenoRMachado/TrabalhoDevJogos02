fd66bf25aa8f505daa46e466296d84eee6bef4c4
Pedro <crawfordpedro@gmail.com>
2025-12-29 18:07:45 -0300

    Implementa lógica boss

---
+++ inimigo_boss.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+@export var distância : float
+@export var velocidade : float
+
+@onready var sprite = $AnimatedSprite2D
+@onready var timer = $Timer
+
+var posicao_inicial
+var auxiliar = -1
+var verificador_timer = false
+var ataque_inimigo = false
+
+func _ready() -> void:
+	posicao_inicial = position.x
+	sprite.play("default")
+	timer.start(2.0)
+
+func _process(delta: float) -> void:
+	if verificador_timer == true:
+		if position.x < posicao_inicial + distância and auxiliar > 0:
+			position.x += auxiliar * velocidade * delta
+		elif position.x > posicao_inicial - distância and auxiliar < 0:
+			position.x += auxiliar * velocidade * delta
+		else:
+			posicao_inicial = position.x
+			auxiliar *= -1
+			if sprite.flip_h == false:
+				sprite.flip_h = true
+			else:
+				sprite.flip_h = false
+			sprite.play("atack")
+			ataque_inimigo = true
+			verificador_timer = false
+			timer.start(3.0)
+
+func _on_timer_timeout() -> void:
+	verificador_timer = true
+	ataque_inimigo = false
+	sprite.play("walking")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "atack":
+		sprite.play("default")
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	pass
