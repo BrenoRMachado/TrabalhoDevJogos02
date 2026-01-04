d7df8b8220449254748fdc3fabb2aec83aff4cfc
FelipeVStrehle <felipe.strehle@estudante.ufjf.br>
2026-01-04 20:50:11 -0300

    Tela créditos

--- menu_principal.gd
+++ menu_principal.gd
@@ -4,6 +4,8 @@ extends Control
 @export var cena_do_jogo: PackedScene
 # Variavel para chamar a tela de como jogar
 @export var cena_como_jogar: PackedScene
+# Variavel para chamar a tela de créditos
+@export var cena_creditos: PackedScene
 
 func _ready():
 	
@@ -13,8 +15,11 @@ func _ready():
 	# Quando apertar Sair
 	$VBoxContainer/BotaoSair.pressed.connect(sair_do_jogo)
 	
-	#	Quando aperta no como jogar
+	# Quando aperta no como jogar
 	$VBoxContainer/BotaoComoJogar.pressed.connect(abrir_como_jogar)
+	
+	# Quando aperta no créditos
+	$VBoxContainer/BotaoCreditos.pressed.connect(abrir_creditos)
 
 func entrar_no_jogo():
 	if cena_do_jogo:
@@ -27,6 +32,12 @@ func abrir_como_jogar():
 		get_tree().change_scene_to_packed(cena_como_jogar)
 	else:
 		print("ERRO: Nenhuma cena de como jogar foi configurada no Inspetor.")
+
+func abrir_creditos():
+	if cena_creditos:
+		get_tree().change_scene_to_packed(cena_creditos)
+	else:
+		print("ERRO: Nenhuma cena de créditos foi configurada no Inspetor.")
 
 func sair_do_jogo():
 	# Fecha o jogo
